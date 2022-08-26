7289c60b8beee94fbbd8eab5d2fe824967a815fe
Jeferson Junio Menchik <juniomenchik@live.com>
2022-08-26 15:56:04 -0300

    44-50. Versionamento etc - Metodos de Validacao

--- Microsservicos-do-0-com-Spring-Cloud/foo/src/main/java/br/com/erudio/GreetingController.java
+++ Microsservicos-do-0-com-Spring-Cloud/foo/src/main/java/br/com/erudio/GreetingController.java
@@ -1,5 +1,6 @@
 package br.com.erudio;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,26 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
-        return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    @RequestMapping("/sum/numberOne/numberTwo")
+    public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)){
+            throw new Exception();
+        }
+        Double sum = convertToDouble(numberOne)+convertToDouble(numberTwo);
+        return sum;
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",",".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",",".");
+        return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
     }
 }
