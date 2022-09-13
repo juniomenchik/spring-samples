d8084b81debb89cb2b51eb90f06fb52082c84b0b
juniomenchik <juniomenchik@live.com>
2022-09-13 11:10:01 -0300

    Section 8. 78. Criando e Conectando Microsservicos com Feign

--- Spring-Cloud-Architect/product-catalog-ws/src/main/java/springboot/springapp/productcatalogws/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package springboot.springapp.productcatalogws.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import springboot.springapp.productcatalogws.model.User;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
