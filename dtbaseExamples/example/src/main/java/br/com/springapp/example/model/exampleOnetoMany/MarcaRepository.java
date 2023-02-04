c73dcb048ac20a9d86df3f256bc856ed21c2f890
Jeferson Junio Menchik <juniomenchik@live.com>
2023-02-04 13:27:09 -0300

    master removendo projetos antigos

--- dtbaseExamples/example/src/main/java/br/com/springapp/example/model/exampleOnetoMany/MarcaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.springapp.example.model.exampleOnetoMany;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarcaRepository extends JpaRepository<Marca, Long> {
-}
