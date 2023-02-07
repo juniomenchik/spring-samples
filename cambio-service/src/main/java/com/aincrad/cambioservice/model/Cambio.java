2e95dd0ea8e6edd10042eaa66a9f12dd11cee927
Jeferson Junio Menchik <ctl_jmenchik@uolinc.com>
2023-02-07 16:41:33 -0300

    master 83. em andamento, adicionando flyway ao cambioservice

---
+++ cambio-service/src/main/java/com/aincrad/cambioservice/model/Cambio.java
@@ -0,0 +1,40 @@
+package com.aincrad.cambioservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cambio implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionFactor;
+    private BigDecimal convertedValue;
+    private String environment;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cambio cambio = (Cambio) o;
+        return Objects.equals(id, cambio.id) && Objects.equals(from, cambio.from) && Objects.equals(to, cambio.to) && Objects.equals(conversionFactor, cambio.conversionFactor) && Objects.equals(convertedValue, cambio.convertedValue) && Objects.equals(environment, cambio.environment);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, from, to, conversionFactor, convertedValue, environment);
+    }
+}
