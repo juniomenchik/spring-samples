c55829498bf27e0b29bd66447ee1780c618187ac
Jeferson Junio Menchik <ctl_jmenchik@uolinc.com>
2023-02-13 22:29:16 -0300

    master Adicionado O @Retryable, foi dificil

---
+++ book-service/src/main/java/com/aincrad/bookservice/controller/FooBarController.java
@@ -0,0 +1,26 @@
+package com.aincrad.bookservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("book-service")
+public class FooBarController {
+
+    private Logger logger = LoggerFactory.getLogger(FooBarController.class);
+
+    @GetMapping("/foo-bar")
+    @Retryable()
+    public String fooBar(){
+        logger.info("Request to foo-bar is received!");
+        var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
+        return response.getBody();
+    }
+
+
+}
