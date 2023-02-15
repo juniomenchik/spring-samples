30a3ca7a56c9faf7ae5abdec02aab3296bcf7815
Jeferson Junio Menchik <ctl_jmenchik@uolinc.com>
2023-02-15 09:04:11 -0300

    master Comecando Secao 12. Dockerfiles de alguns nao funcionam

--- book-service/src/main/java/com/aincrad/bookservice/controller/FooBarController.java
+++ book-service/src/main/java/com/aincrad/bookservice/controller/FooBarController.java
@@ -2,7 +2,8 @@ package com.aincrad.bookservice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.annotation.CircuitBreaker;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,17 @@ public class FooBarController {
     private Logger logger = LoggerFactory.getLogger(FooBarController.class);
 
     @GetMapping("/foo-bar")
-    @Retryable()
+    //@Retryable(value = Exception.class)
+    @CircuitBreaker(value = Exception.class)
     public String fooBar(){
         logger.info("Request to foo-bar is received!");
         var response = new RestTemplate().getForEntity("http://localhost:8080/foo-bar", String.class);
         return response.getBody();
     }
 
+    @Recover
+    public String chesquedele(Exception e){
+        return "Chescou ne amigao ";
+    }
 
 }
