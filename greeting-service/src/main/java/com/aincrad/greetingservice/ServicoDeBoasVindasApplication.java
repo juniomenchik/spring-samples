2297d40123af4b31c6b919dba4e6a55261a191a0
Jeferson Junio Menchik <juniomenchik@live.com>
2023-04-12 18:51:58 -0300

    master ainda nao finalizado o docker-compose

--- greeting-service/src/main/java/com/aincrad/greetingservice/ServicoDeBoasVindasApplication.java
+++ greeting-service/src/main/java/com/aincrad/greetingservice/ServicoDeBoasVindasApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServicoDeBoasVindasApplication {
+public class  ServicoDeBoasVindasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicoDeBoasVindasApplication.class, args);
