2f5a8ab2afe57d952564f2df8decaaaec60d3f4c
Jeferson Junio Menchik <juniomenchik@live.com>
2023-09-05 17:50:11 -0300

    master melhorando docker-compose.yaml

--- book-service/src/main/java/com/aincrad/bookservice/controller/BookController.java
+++ book-service/src/main/java/com/aincrad/bookservice/controller/BookController.java
@@ -1,20 +1,20 @@
 package com.aincrad.bookservice.controller;
 
+import java.util.Optional;
+
 import com.aincrad.bookservice.model.Book;
 import com.aincrad.bookservice.repository.BookRepository;
 import com.aincrad.bookservice.response.Cambio;
 import com.aincrad.bookservice.response.CambioProxyFeign;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Objects;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("book-service")
@@ -24,17 +24,18 @@ public class BookController {
     private Environment env;
     @Autowired
     private BookRepository repository;
-
     @Autowired
     private CambioProxyFeign cambioProxyFeign;
     //http://localhost:8000/book-service/1/BRL
+    Logger logger = LogManager.getLogger(BookController.class);
 
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(
             @PathVariable("id") Long id,
             @PathVariable("currency") String currency
     ){
-
+        logger.info("BookController::findBook Iniciando Metodo...");
+        logger.info("BookController::findBook request payload: id: "+id+" currency: "+currency);
         var localPort = env.getProperty("local.server.port");
         Optional<Book> book = repository.findById(id);
         if (book.isEmpty()) throw new RuntimeException("Book not found");
@@ -47,6 +48,7 @@ public class BookController {
         book.get().setPrice(resProxy.getConvertedValue());
         book.get().setCurrency(currency);
 
+        logger.info("BookController::findBook response payload: "+ book.get());
         return book.get();
     }
 
