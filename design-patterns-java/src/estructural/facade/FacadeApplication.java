9d6460e216522df1ebf0060ed4ec41856ac44429
Jeferson Junio Menchik <juniomenchik@live.com>
2023-09-21 08:09:56 -0300

    master design patterns Decorator and Facade

--- design-patterns-java/src/estructural/facade/FacadeApplication.java
+++ design-patterns-java/src/estructural/facade/FacadeApplication.java
@@ -1,2 +1,12 @@
-package estructural.facade;public class FacadeApplication {
+package estructural.facade;
+
+import creational.builder.interfaces.ProdutoBuilderProtocol;
+
+public class FacadeApplication {
+    public static void main(String[] args) {
+        FacadeBuilder facadeBuilder = new FacadeBuilder();
+        System.out.println(facadeBuilder.makeCompleteMeal().getPrice());
+        System.out.println(facadeBuilder.makeDrinks().getPrice());
+        System.out.println(facadeBuilder.makeRice().getPrice());
+    }
 }
